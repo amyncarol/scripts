3a763f77ac5c2ea8e32873a989186568450fac38
Yao Cai <yaocai@ln001.brc>
2018-04-06 13:52:16 -0700

    savio submit.sh

--- sh_script/savio-submit/submit_gpu.sh
+++ sh_script/savio-submit/submit_gpu.sh
@@ -1,30 +1,19 @@
 #!/bin/bash
-# Job name:
-#SBATCH --job-name=test
-#
-# Account:
-#SBATCH --account=account_name
-#
-# Partition:
+#SBATCH --job-name=gpu_test
+#SBATCH --account=fc_mdasta
 #SBATCH --partition=savio2_gpu
-#
-# QoS:
-#SBATCH --qos=qos_name
-#
-# Number of nodes:
+#SBATCH --time=00:30:00
+
 #SBATCH --nodes=1
-#
-# Number of tasks (one for each GPU desired for use case) (example):
-#SBATCH --ntasks=1
-#
-# Processors per task (please always specify the total number of processors twice the number of GPUs):
-#SBATCH --cpus-per-task=2
-#
+
 #Number of GPUs, this can be in the format of "gpu:[1-4]", or "gpu:K80:[1-4] with the type included
 #SBATCH --gres=gpu:1
-#
-# Wall clock limit:
-#SBATCH --time=00:00:30
-#
-## Command(s) to run (example):
+
+# Number of tasks (one for each GPU desired for use case) (example):
+#SBATCH --ntasks=1
+
+# Processors per task (please always specify the total number of processors twice the number of GPUs):
+#SBATCH --cpus-per-task=2
+
+module load ml/tensorflow/1.0.0-py36
 ./a.out
